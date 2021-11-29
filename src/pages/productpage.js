faf1e1b18babdab24c3dbc6ff21f9bf13059d7fe
Liptai Klaudia <klaudia@insine.email>
2021-11-29 16:18:05 +0100

    product page done

--- src/pages/productpage.js
+++ src/pages/productpage.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
-import { data } from './productsData'
+import React, { Component } from "react";
+import Header from "../components/header";
+import { data } from "./productsData";
 
 export default class Productpage extends Component {
-    render() {
-        const product = data[this.props.id]
-        return (
-            <div>
-                <h1>{product.title}</h1>
-                <h3>{product.price}</h3>
-            </div>
-        )
-    }
+  render() {
+    const product = data[this.props.id];
+    return (
+      <>
+        <Header />
+        <div id="productData">
+          <img src={`/assets/images/frog-${Number(this.props.id) + 1}.png`} />
+          <div>
+            <h1>{product.title}</h1>
+            <h3>${(Number(product.price) / 100).toFixed(2)}</h3>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
